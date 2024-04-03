3b1d15aadee250910ed0fdec62cf0decc2450bb4
ktiriveedi <ktiriveedi@gmail.com>
2024-04-03 14:43:43 -0500

    Update and rename deploy.ps1 to stop.ps1

--- scripts/stop.ps1
+++ scripts/stop.ps1
@@ -5,9 +5,3 @@ Stop-Service -Name w3svc -Force
 
 # Delete existing files
 Remove-Item -Path C:\inetpub\wwwroot\* -Recurse -Force
-
-# Copy new files
-#Copy-Item -Path $env:CODEDEPLOY_DEPLOYMENT_GROUP_ID\* -Destination C:\inetpub\wwwroot\ -Recurse
-
-# Start the service
-Start-Service -Name w3svc
