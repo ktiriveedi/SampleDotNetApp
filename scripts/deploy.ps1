b1f111d5a8f190053629df0d10d20d6ab5d21947
ktiriveedi <ktiriveedi@gmail.com>
2024-04-03 13:25:41 -0500

    Update deploy.ps1

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,14 +1,13 @@
 # Deployment script for SampleDotNetApp
 
 # Stop the service if running
-Stop-Service -Name SampleDotNetApp -Force
+Stop-Service -Name w3svc -Force
 
 # Delete existing files
-Remove-Item -Path C:\inetpub\wwwroot\SampleDotNetApp\* -Recurse -Force
+Remove-Item -Path C:\inetpub\wwwroot\* -Recurse -Force
 
 # Copy new files
-Copy-Item -Path $env:CODEDEPLOY_DEPLOYMENT_GROUP_ID\* -Destination C:\inetpub\wwwroot\SampleDotNetApp\ -Recurse
+Copy-Item -Path $env:CODEDEPLOY_DEPLOYMENT_GROUP_ID\* -Destination C:\inetpub\wwwroot\ -Recurse
 
 # Start the service
-Start-Service -Name SampleDotNetApp
-
+Start-Service -Name w3svc
