14708a22cc972c69786c80b2e8026aa74f6f55a8
ktiriveedi <ktiriveedi@gmail.com>
2024-04-03 15:20:55 -0500

    Update stop.ps1

--- scripts/stop.ps1
+++ scripts/stop.ps1
@@ -7,18 +7,15 @@ Stop-Service -Name w3svc -Force
 Remove-Item -Path C:\inetpub\wwwroot\* -Recurse -Force
 
 
-# Copy new files
-
 # Get the source directory from an environment variable
-$sourceDirectory = $env:CODEBUILD_SRC_DIR\publish_output
+$sourceDirectory = "$env:CODEBUILD_SRC_DIR\\publish_output"
 
 # Destination directory
-$destinationDirectory = "C:\inetpub\wwwroot"
+$destinationDirectory = "C:\\inetpub\\wwwroot"
 
 # Copy files from source to destination
-Copy-Item -Path $sourceDirectory\* -Destination $destinationDirectory -Recurse -Force
+Copy-Item -Path $sourceDirectory\\* -Destination $destinationDirectory -Recurse -Force
 
-#Copy-Item -Path $env:CODEDEPLOY_DEPLOYMENT_GROUP_ID\* -Destination C:\inetpub\wwwroot\ -Recurse
 
 # Start the service
 Start-Service -Name w3svc
