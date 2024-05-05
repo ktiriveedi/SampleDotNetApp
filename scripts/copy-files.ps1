4d7a195a8cd2e6534cd30a49de5b5b57906c6947
kondaiah <ktiriveedi@gmail.com>
2024-05-05 18:07:43 -0500

    addes copy files script - fdfef

--- scripts/copy-files.ps1
+++ scripts/copy-files.ps1
@@ -1,9 +1,6 @@
-Write-Host "Root path Directory of the scritp: $PSScriptRoot"
+Write-Host "Root path Directory of the script: $PSScriptRoot"
 $deploymentDir = Split-Path -Parent $PSScriptRoot
 Write-Host "Deployment Directory: $deploymentDir"
-
-#$sourceDir = Join-Path -Path $deploymentDir "
-#Write-Host "Source Directory: $sourceDir"
 
 $destinationDir = "C:\liquibase"
 Write-Host "Destination Directory: $destinationDir"
@@ -24,4 +21,21 @@ if (Test-Path -Path $sourceFile) {
 }
 else {
     Write-Warning "The source file '$sourceFile' does not exist in the deployment directory."
+}
+
+# Copy the "db" directory
+$dbSourceDir = Join-Path -Path $deploymentDir "db"
+Write-Host "DB Source Directory: $dbSourceDir"
+
+$dbDestinationDir = Join-Path -Path $destinationDir "db"
+Write-Host "DB Destination Directory: $dbDestinationDir"
+
+# Check if the "db" source directory exists in the deployment directory
+if (Test-Path -Path $dbSourceDir) {
+    # Copy the entire "db" directory from the deployment directory to the destination, overwriting existing files
+    Copy-Item -Path $dbSourceDir -Destination $dbDestinationDir -Recurse -Force
+    Write-Host "The directory '$dbSourceDir' has been copied to the destination directory."
+}
+else {
+    Write-Warning "The source directory '$dbSourceDir' does not exist in the deployment directory."
 }
