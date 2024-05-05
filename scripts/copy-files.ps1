3610f2f1140a0b6eba9e3efeab578b90e5b3af41
kondaiah <ktiriveedi@gmail.com>
2024-05-05 17:24:41 -0500

    addes copy files script

--- scripts/copy-files.ps1
+++ scripts/copy-files.ps1
@@ -1,18 +1,27 @@
-$sourceDir = "C:\codebuild\artifacts\publish_output"
-$destinationDir = "C:liquibase"
+Write-Host "Root path Directory of the scritp: $PSScriptRoot"
+$deploymentDir = Split-Path -Parent $PSScriptRoot
+Write-Host "Deployment Directory: $deploymentDir"
 
-# Path to the specific file in the CodeBuild bundle
+$sourceDir = Join-Path -Path $deploymentDir -ChildPath "publish_output"
+Write-Host "Source Directory: $sourceDir"
+
+$destinationDir = "C:\liquibase"
+Write-Host "Destination Directory: $destinationDir"
+
+# Path to the specific file in the deployment directory
 $sourceFile = Join-Path -Path $sourceDir -ChildPath "liquibase.properties"
+Write-Host "Source File: $sourceFile"
 
-# Path to the specific location where the file will be replaced
+# Path to the specific location where the file will be copied
 $destinationFile = Join-Path -Path $destinationDir -ChildPath "liquibase.properties"
+Write-Host "Destination File: $destinationFile"
 
-# Check if the source file exists in the CodeBuild bundle
+# Check if the source file exists in the deployment directory
 if (Test-Path -Path $sourceFile) {
-    # Copy the specific file from the CodeBuild bundle to the destination, overwriting the existing file if it exists
+    # Copy the specific file from the deployment directory to the destination, overwriting the existing file if it exists
     Copy-Item -Path $sourceFile -Destination $destinationFile -Force
-    Write-Host "The file '$sourceFile' has been replaced in the destination directory."
+    Write-Host "The file '$sourceFile' has been copied to the destination directory."
 }
 else {
-    Write-Warning "The source file '$sourceFile' does not exist in the CodeBuild bundle."
+    Write-Warning "The source file '$sourceFile' does not exist in the deployment directory."
 }
